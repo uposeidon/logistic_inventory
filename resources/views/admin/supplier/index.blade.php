d006ebbcdfd7869f08dcc9847b29fcdb42a240ef
uposeidon <greatcgdev007@gmail.com>
2021-01-04 18:23:24 +0500

    Analyze button hide from origin manifest page

--- resources/views/admin/supplier/index.blade.php
+++ resources/views/admin/supplier/index.blade.php
@@ -15,7 +15,7 @@
                </div>
                <div class="form-group col-md-4">
                   <button type="submit" class="btn btn-primary">Search</button>
-                  <a class="btn btn-primary" href="{{ route('admin.analyze.index') }}">Analyze</a>
+                  <!-- <a class="btn btn-primary" href="{{ route('admin.analyze.index') }}">Analyze</a> -->
                </div>
             </div>
             </form>
