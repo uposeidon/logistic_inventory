371e61f1ed1c769e99ec2401b4d72a8edcd30d01
uposeidon <greatcgdev007@gmail.com>
2021-01-17 03:32:51 +0600

    Frontend in Under Development

---
+++ frontend_app/src/components/partials/Banner.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="banner-area">
+      <div class="banner structure">
+        <h2 class="product-title">WOLFGANG PUCK-Organic Soup - Free Range Chicken Noodle, 1 ( 14.5 OZ )</h2>
+        <div class="product-info">
+          <div class="product product-1">
+            <span>Highest Profit Opportunity &nbsp;&nbsp;&nbsp;<i class="fa fa-question-circle"></i></span>
+            <h5>Amazon CA$Nan</h5>
+          </div>
+          <div class="product product-2">
+            <span>Product's Demand Level &nbsp;&nbsp;&nbsp;<i class="fa fa-question-circle"></i></span>
+            <!-- <h5>Amazon CA$Nan</h5> -->
+            <h1>N/A</h1>
+          </div>
+          <div class="product product-3">
+            <span>Expenses &nbsp;&nbsp;&nbsp;<i class="fa fa-question-circle"></i></span>
+            <!-- <h5>Amazon CA$Nan</h5> -->
+            <h1>N/A</h1>
+          </div>
+          <div class="product product-4">
+            <span>Profit &nbsp;&nbsp;&nbsp;<i class="fa fa-question-circle"></i></span>
+            <!-- <h5>Amazon CA$Nan</h5> -->
+            <h1>N/A</h1>
+          </div>
+          <div class="product product-5">
+            <!-- <div class="product-status-not-rec">
+                <span>Not <b>Recommended</b></span>
+            </div>
+            <span class="status-mesg">Couldn’t Located Market price</span> -->
+            <div class="product-status-rec">
+                <span>Product <b>Recommended</b></span>
+            </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import "../../assets/css/banner.css";
+
+export default {
+  name: "Banner",
+};
+</script>
