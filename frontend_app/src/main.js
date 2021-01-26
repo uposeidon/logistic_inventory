7ca2c8d7d5e58f99a624ce7d9883c273d89367cd
Tom C <tom.c@email.com>
2021-01-27 01:56:58 +0800

    Sales Breakdown

--- frontend_app/src/main.js
+++ frontend_app/src/main.js
@@ -1,6 +1,20 @@
 import { createApp } from 'vue';
 import App from './App.vue'
 import '@/assets/sass/app.sass'
+import Emitter from 'tiny-emitter'
 
 const app = createApp(App)
+
+const emitter = new Emitter()
+
+app.config.globalProperties.$eventBus = {
+  $emit: (eventName, ...args) => {
+    emitter.emit(eventName, ...args)
+  },
+  $register: (eventName, cb) => {
+    emitter.off(eventName)
+    emitter.on(eventName, cb)
+  }
+}
+
 app.mount('#app')
